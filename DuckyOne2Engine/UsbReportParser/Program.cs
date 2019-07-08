76cd5f5903867ba8f20d2bbb1d465b4ded548786
YI ZHANG <scaccterran@gmail.com>
2019-07-08 13:29:19 -0400

    parsed open/close commands

--- DuckyOne2Engine/UsbReportParser/Program.cs
+++ DuckyOne2Engine/UsbReportParser/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace UsbReportParser
 {
@@ -10,6 +7,19 @@ namespace UsbReportParser
     {
         static void Main(string[] args)
         {
+            try
+            {
+                var parser = new Parser();
+                var file = File.ReadAllText("raw_report.txt");
+                var input = parser.GetInput(file);
+                var output = parser.GetOutput(file);
+                File.WriteAllLines("input.txt", input);
+                File.WriteAllLines("output.txt", output);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
