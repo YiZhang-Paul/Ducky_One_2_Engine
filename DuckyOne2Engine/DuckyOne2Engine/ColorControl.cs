0d70464b9ecc064bb44bb1438ed7822bbc83c016
YI ZHANG <scaccterran@gmail.com>
2019-07-08 17:15:05 -0400

    color controller

---
+++ DuckyOne2Engine/DuckyOne2Engine/ColorControl.cs
@@ -0,0 +1,115 @@
+﻿using DuckyOne2Engine.KeyColors;
+using HidLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DuckyOne2Engine
+{
+    public class ColorControl
+    {
+        private IHidDevice Device { get; }
+        private IKeyColorMapper Mapper { get; }
+        private byte[][] CurrentReport { get; }
+
+        private byte[][] BlankReport
+        {
+            get
+            {
+                var report = new List<byte[]>();
+                AddOpenRequest(report);
+                AddColorRequests(report);
+                AddCloseRequest(report);
+
+                return report.ToArray();
+            }
+        }
+
+        public ColorControl(IHidDevice device, IKeyColorMapper mapper)
+        {
+            Device = device;
+            Mapper = mapper;
+            CurrentReport = BlankReport;
+        }
+
+        public void SetColor(Tuple<string, byte[]> color)
+        {
+            var positions = Mapper.GetBytePositions(color.Item1).ToArray();
+
+            if (!positions.Any())
+            {
+                return;
+            }
+
+            for (int i = 0; i < positions.Length; ++i)
+            {
+                var row = positions[i].Row + 1;
+                var index = positions[i].Index;
+                CurrentReport[row][index] = color.Item2[i];
+            }
+        }
+
+        public void SetColors(IEnumerable<Tuple<string, byte[]>> colors)
+        {
+            foreach (var color in colors)
+            {
+                SetColor(color);
+            }
+        }
+
+        public void ApplyColors()
+        {
+            foreach (var input in CurrentReport)
+            {
+                Device.Write(input);
+            }
+        }
+
+        private void AddOpenRequest(List<byte[]> report)
+        {
+            byte[] request = new byte[65];
+            request[1] = 0x56;
+            request[2] = 0x81;
+            request[5] = 0x01;
+            request[9] = 0x07;
+            request[13] = 0xAA;
+            request[14] = 0xAA;
+            request[15] = 0xAA;
+            request[16] = 0xAA;
+            report.Add(request);
+        }
+
+        private void AddColorRequests(List<byte[]> report)
+        {
+            var requests = new List<byte[]>();
+
+            for (int i = 0; i < 7; ++i)
+            {
+                byte[] request = new byte[65];
+                request[1] = 0x56;
+                request[2] = 0x83;
+                request[3] = Convert.ToByte(i);
+                requests.Add(request);
+            }
+
+            requests[0][5] = 0x01;
+            requests[0][9] = 0x80;
+            requests[0][10] = 0x01;
+            requests[0][12] = 0xC1;
+            requests[0][17] = 0xFF;
+            requests[0][18] = 0xFF;
+            requests[0][19] = 0xFF;
+            requests[0][20] = 0xFF;
+            report.AddRange(requests);
+        }
+
+        private void AddCloseRequest(List<byte[]> report)
+        {
+            byte[] request = new byte[65];
+            request[1] = 0x51;
+            request[2] = 0x28;
+            request[5] = 0xFF;
+            report.Add(request);
+        }
+    }
+}
---
+++ DuckyOne2Engine/DuckyOne2EngineTest/UnitTests/ColorControlTest.cs
@@ -0,0 +1,183 @@
+﻿using DuckyOne2Engine;
+using DuckyOne2Engine.KeyColors;
+using HidLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace DuckyOne2EngineTest.UnitTests
+{
+    [TestClass]
+    public class ColorControlTest
+    {
+        private Tuple<string, byte[]>[] _colors;
+        private Mock<IHidDevice> _device;
+        private Mock<IKeyColorMapper> _mapper;
+        private ColorControl _control;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _colors = new[]
+            {
+                new Tuple<string, byte[]>("G", new byte[] {0, 0, 255}),
+                new Tuple<string, byte[]>("5", new byte[] {255, 255, 0}),
+                new Tuple<string, byte[]>("SPACE", new byte[] {255, 0, 255})
+            };
+
+            _device = new Mock<IHidDevice>();
+            _mapper = new Mock<IKeyColorMapper>();
+            _control = new ColorControl(_device.Object, _mapper.Object);
+        }
+
+        [TestMethod]
+        public void SetColorShouldNotSendRequestsToUsbDevice()
+        {
+            _control.SetColor(_colors[0]);
+
+            _device.Verify(x => x.Write(It.IsAny<byte[]>()), Times.Never);
+        }
+
+        [TestMethod]
+        public void SetColorShouldSetColorOnReport()
+        {
+            var color = _colors[1];
+            var bytes = new StringBuilder();
+
+            var expected = new []
+            {
+                "00 56 81 00 00 01 00 00 00 07 00 00 00 AA AA AA AA 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 56 83 00 00 01 00 00 00 80 01 00 C1 00 00 00 00 FF FF FF FF 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 56 83 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 FF FF 00 00 00 00 00",
+                "00 56 83 02 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 56 83 03 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 56 83 04 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 56 83 05 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 56 83 06 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 51 28 00 00 FF 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"
+            };
+
+            _mapper.Setup(x => x.GetBytePositions(color.Item1))
+                .Returns(new []
+                {
+                    new BytePosition(1, 58),
+                    new BytePosition(1, 59),
+                    new BytePosition(1, 60)
+                });
+
+            _device.Setup(x => x.Write(It.IsAny<byte[]>()))
+                .Callback((byte[] input) =>
+                {
+                    var hex = input.Select(_ => _.ToString("X2"));
+                    bytes.Append($" {string.Join(" ", hex)}");
+                })
+                .Returns(true);
+
+            _control.SetColor(color);
+            _control.ApplyColors();
+
+            Assert.AreEqual(string.Join(" ", expected), bytes.ToString().Trim());
+        }
+
+        [TestMethod]
+        public void SetColorsShouldNotSendRequestsToUsbDevice()
+        {
+            _control.SetColors(_colors);
+
+            _device.Verify(x => x.Write(It.IsAny<byte[]>()), Times.Never);
+        }
+
+        [TestMethod]
+        public void SetColorsShouldSetColorsOnReport()
+        {
+            var bytes = new StringBuilder();
+
+            var expected = new[]
+            {
+                "00 56 81 00 00 01 00 00 00 07 00 00 00 AA AA AA AA 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 56 83 00 00 01 00 00 00 80 01 00 C1 00 00 00 00 FF FF FF FF 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 56 83 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 FF FF 00 00 00 00 00",
+                "00 56 83 02 00 00 FF 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 FF 00 FF 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 56 83 03 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 56 83 04 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 56 83 05 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 56 83 06 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 51 28 00 00 FF 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"
+            };
+
+            _mapper.SetupSequence(x => x.GetBytePositions(It.IsAny<string>()))
+                .Returns(new[]
+                {
+                    new BytePosition(1, 64),
+                    new BytePosition(2, 5),
+                    new BytePosition(2, 6)
+                })
+                .Returns(new[]
+                {
+                    new BytePosition(1, 58),
+                    new BytePosition(1, 59),
+                    new BytePosition(1, 60)
+                })
+                .Returns(new[]
+                {
+                    new BytePosition(2, 28),
+                    new BytePosition(2, 29),
+                    new BytePosition(2, 30)
+                });
+
+            _device.Setup(x => x.Write(It.IsAny<byte[]>()))
+                .Callback((byte[] input) =>
+                {
+                    var hex = input.Select(_ => _.ToString("X2"));
+                    bytes.Append($" {string.Join(" ", hex)}");
+                })
+                .Returns(true);
+
+            _control.SetColors(_colors);
+            _control.ApplyColors();
+
+            Assert.AreEqual(string.Join(" ", expected), bytes.ToString().Trim());
+        }
+
+        [TestMethod]
+        public void ApplyColorsShouldSendRequestsToUsbDevice()
+        {
+            _control.ApplyColors();
+
+            _device.Verify(x => x.Write(It.IsAny<byte[]>()), Times.Exactly(9));
+        }
+
+        [TestMethod]
+        public void ApplyColorsShouldSendBlankReportOnDefault()
+        {
+            var bytes = new StringBuilder();
+
+            var expected = new[]
+            {
+                "00 56 81 00 00 01 00 00 00 07 00 00 00 AA AA AA AA 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 56 83 00 00 01 00 00 00 80 01 00 C1 00 00 00 00 FF FF FF FF 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 56 83 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 56 83 02 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 56 83 03 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 56 83 04 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 56 83 05 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 56 83 06 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                "00 51 28 00 00 FF 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"
+            };
+
+            _device.Setup(x => x.Write(It.IsAny<byte[]>()))
+                .Callback((byte[] input) =>
+                {
+                    var hex = input.Select(_ => _.ToString("X2"));
+                    bytes.Append($" {string.Join(" ", hex)}");
+                })
+                .Returns(true);
+            
+            _control.ApplyColors();
+
+            Assert.AreEqual(string.Join(" ", expected), bytes.ToString().Trim());
+        }
+    }
+}
