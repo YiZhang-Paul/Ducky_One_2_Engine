b881ad2ce782da55c280040d9ed61e7a2e81e74a
YI ZHANG <scaccterran@gmail.com>
2019-07-13 19:04:33 -0400

    setup ducky devices

---
+++ DuckyOne2Engine/DuckyOne2Engine/DuckyDevices/DuckyDevice.cs
@@ -0,0 +1,50 @@
+﻿using DuckyOne2Engine.HidDevices;
+using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace DuckyOne2Engine.DuckyDevices
+{
+    public class DuckyDevice
+    {
+        private IHidDevice Device { get; }
+
+        public DuckyDevice(IHidDevice device, Action onExit)
+        {
+            Device = device;
+            Open();
+            Thread.Sleep(1500);
+            Setup(onExit);
+        }
+
+        private void Setup(Action onExit)
+        {
+            Hook.GlobalEvents().OnCombination(new Dictionary<Combination, Action>
+            {
+                { Combination.FromString("Control+Shift+M"), onExit }
+            });
+        }
+
+        public void Open()
+        {
+            SendCommandFromFile(Device, "../Commands/open.txt");
+        }
+
+        public void Close()
+        {
+            SendCommandFromFile(Device, "Commands/open.txt");
+        }
+
+        private void SendCommandFromFile(IHidDevice device, string name)
+        {
+            foreach (var line in File.ReadAllLines(name))
+            {
+                var hex = line.Trim().Split(' ');
+                device.Write(hex.Select(_ => Convert.ToByte(_, 16)).ToArray());
+            }
+        }
+    }
+}
