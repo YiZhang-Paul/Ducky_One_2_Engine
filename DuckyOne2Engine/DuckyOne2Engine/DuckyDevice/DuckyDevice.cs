26b8e0d293986a76df4a7ba261999eac27818c78
YI ZHANG <scaccterran@gmail.com>
2019-07-13 18:54:34 -0400

    ducky device

---
+++ DuckyOne2Engine/DuckyOne2Engine/DuckyDevice/DuckyDevice.cs
@@ -0,0 +1,29 @@
+﻿using DuckyOne2Engine.HidDevices;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace DuckyOne2Engine.DuckyDevice
+{
+    public class DuckyDevice
+    {
+        private IHidDevice Device { get; }
+
+        public DuckyDevice(IHidDevice device)
+        {
+            Device = device;
+            SendCommandFromFile(Device, "../Commands/open.txt");
+            Thread.Sleep(1500);
+        }
+
+        private void SendCommandFromFile(IHidDevice device, string name)
+        {
+            foreach (var line in File.ReadAllLines(name))
+            {
+                var hex = line.Trim().Split(' ');
+                device.Write(hex.Select(_ => Convert.ToByte(_, 16)).ToArray());
+            }
+        }
+    }
+}
