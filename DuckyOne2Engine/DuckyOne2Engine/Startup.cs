3754dd17d774d768512672abccdd8061a8ac7633
YI ZHANG <scaccterran@gmail.com>
2019-07-14 18:15:08 -0400

    color mode api

--- DuckyOne2Engine/DuckyOne2Engine/Startup.cs
+++ DuckyOne2Engine/DuckyOne2Engine/Startup.cs
@@ -11,8 +11,26 @@ namespace DuckyOne2Engine
 
             config.Routes.MapHttpRoute
             (
-                "SetMode",
-                "api/v1/{controller}/{mode}/{backRgb}"
+                "SetReactiveMode",
+                "api/v1/{controller}/reactive/{backRgb}/{activeRgb}/{steps}"
+            );
+
+            config.Routes.MapHttpRoute
+            (
+                "SetBlinkMode",
+                "api/v1/{controller}/blink/{backRgb}/{interval}"
+            );
+
+            config.Routes.MapHttpRoute
+            (
+                "SetBreathMode",
+                "api/v1/{controller}/breath/{backRgb}/{steps}"
+            );
+
+            config.Routes.MapHttpRoute
+            (
+                "SetSprintMode",
+                "api/v1/{controller}/sprint/{backRgb}/{sprintRgb}/{speed}"
             );
 
             builder.UseWebApi(config);
---
+++ DuckyOne2Engine/DuckyOne2Engine/WebApis/ColorModeController.cs
@@ -0,0 +1,72 @@
+﻿using DuckyOne2Engine.DuckyDevices;
+using DuckyOne2Engine.DuckyDevices.ColorModes;
+using System;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web.Http;
+
+namespace DuckyOne2Engine.WebApis
+{
+    public class ColorModeController : ApiController
+    {
+        private DuckyDevice Device { get; }
+
+        public ColorModeController() : this(Cache.ActiveDuckyDevice) { }
+
+        public ColorModeController(DuckyDevice device)
+        {
+            Device = device;
+        }
+
+        private bool IsValidRgb(string rgb)
+        {
+            return Regex.IsMatch(rgb, @"^\d+-\d+-\d+$");
+        }
+
+        private byte[] ParseRgb(string rgb)
+        {
+            return rgb.Split('-').Select(_ => Convert.ToByte(_)).ToArray();
+        }
+
+        public void PostReactiveMode(string backRgb, string activeRgb, int steps)
+        {
+            if (!IsValidRgb(backRgb) || !IsValidRgb(activeRgb))
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            Device.Use(new ReactiveMode(ParseRgb(backRgb), ParseRgb(activeRgb), steps));
+        }
+
+        public void PostBlinkMode(string backRgb, int interval)
+        {
+            if (!IsValidRgb(backRgb))
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            Device.Use(new BlinkMode(ParseRgb(backRgb), interval));
+        }
+
+        public void PostBreathMode(string backRgb, int steps)
+        {
+            if (!IsValidRgb(backRgb))
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            Device.Use(new BreathMode(ParseRgb(backRgb), steps));
+        }
+
+        public void PostSprintMode(string backRgb, string sprintRgb, int speed)
+        {
+            if (!IsValidRgb(backRgb) || !IsValidRgb(sprintRgb))
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            Device.Use(new SprintMode(ParseRgb(backRgb), ParseRgb(sprintRgb), speed));
+        }
+    }
+}
